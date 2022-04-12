158d04b8e528e382d637f2ad1af38e00556c907e
barreto <gbarreto@keener.io>
2022-04-11 23:02:38 -0300

    tabela de perguntas

--- src/components/dashboard/DashboardHome.vue
+++ src/components/dashboard/DashboardHome.vue
@@ -28,34 +28,45 @@
         </div>
       </div>
     </nav>
-    <div class="grid place-items-center h-screen">
-      <p class="text-2xl text-zinc-100">Dashboard</p>
-      <table class="table-auto text-zinc-100">
-        <thead>
-          <tr>
-            <th>Song</th>
-            <th>Artist</th>
-            <th>Year</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td>Malcolm Lockyer</td>
-            <td>1961</td>
-          </tr>
-          <tr>
-            <td>Witchy Woman</td>
-            <td>The Eagles</td>
-            <td>1972</td>
-          </tr>
-          <tr>
-            <td>Shining Star</td>
-            <td>Earth, Wind, and Fire</td>
-            <td>1975</td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="h-screen">
+      <div class="mt-10 text-zinc-100 text-center text-lg font-bold">
+        <p>Ativas<span class="ml-14">Pendentes</span></p>
+      </div>
+      <div class="mt-5 overflow-x-auto rounded-xl">
+        <table class="w-full text-sm text-left text-zinc-100">
+          <thead class="text-xs text-zinc-100 uppercase bg-zinc-700">
+            <tr>
+              <th scope="col" class="px-6 py-3">ID</th>
+              <th scope="col" class="px-6 py-3">Cadastrado por</th>
+              <th scope="col" class="px-6 py-3">Personagem/Autor</th>
+              <th scope="col" class="px-6 py-3">Status</th>
+              <th scope="col" class="px-6 py-3">
+                <span class="sr-only">Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="i in 10"
+              :key="i"
+              class="bg-zinc-100 border-b-2 border-zinc-400 text-zinc-900"
+            >
+              <th
+                scope="row"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+              >
+                Apple MacBook Pro 17"
+              </th>
+              <td class="px-6 py-4">Sliver</td>
+              <td class="px-6 py-4">Laptop</td>
+              <td class="px-6 py-4">$2999</td>
+              <td class="px-6 py-4 text-right text-violet-600">
+                <p>Editar</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
